32912bc3adc73df9f64ae4b6f8dec3c895f68241
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-09-26 06:36:02 -0600

    Patch: attachdirection

--- config.h
+++ config.h
@@ -41,9 +41,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact               = 0.55 ; /* factor of master area size [0.05..0.95] */
+static int nmaster               = 1    ; /* number of clients in master area */
+static int resizehints           = 1    ; /* 1 means respect size hints in tiled resizals */
+static const int attachdirection = 3    ; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 #include "fibonacci.c"
 static const Layout layouts[] = {
