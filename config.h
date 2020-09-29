15572aa60eb72f64a25c118ed8fe612c24897fde
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-09-28 23:47:24 -0600

    Refactor: tabularized format

--- config.h
+++ config.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const int gappx     = 5;                 /* gaps between windows */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static const int gappx           = 5;                 /* gaps between windows */
+static const int swallowfloating = 0;        /* 1 means swallow floating windows by default */
+static unsigned int borderpx     = 1;        /* border pixel of windows */
+static unsigned int snap         = 32;       /* snap pixel */
+static int showbar               = 1;        /* 0 means no bar */
+static int topbar                = 1;        /* 0 means bottom bar */
 static const char *fonts[]       = { "FiraCode Nerd Font:size=12" };
 static const char dmenufont[]    = "FiraCode Nerd Font:size=12";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char normbgcolor[]        = "#222222";
+static char normbordercolor[]    = "#444444";
+static char normfgcolor[]        = "#bbbbbb";
+static char selfgcolor[]         = "#eeeeee";
+static char selbordercolor[]     = "#005577";
+static char selbgcolor[]         = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       [SchemeNorm] = { normfgcolor , normbgcolor , normbordercolor } ,
+       [SchemeSel]  = { selfgcolor  , selbgcolor  , selbordercolor  } ,
 };
 
 /* tagging */
@@ -38,17 +38,17 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact               = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster               = 1;    /* number of clients in master area */
+static int resizehints           = 1;    /* 1 means respect size hints in tiled resizals */
 static const int attachdirection = 3;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 static const Layout layouts[] = {
-    { "﩯"   , tile    }                , /* first entry is default */
-    { ""   , NULL    }                , /* no layout function means floating behavior */
-    { ""   , monocle }                ,
-    { "[D]" , deck    }                ,
-    { "יִ"   , centeredmaster }         ,
+    { "﩯"   , tile                   } , /* first entry is default */
+    { ""   , NULL                   } , /* no layout function means floating behavior */
+    { ""   , monocle                } ,
+    { "[D]" , deck                   } ,
+    { "יִ"   , centeredmaster         } ,
     { ""   , centeredfloatingmaster } ,
 };
 
@@ -72,53 +72,54 @@ static const char *termcmd[]  = { "st", NULL };
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+		{ "normbgcolor"     , STRING  , &normbgcolor     },
+		{ "normbordercolor" , STRING  , &normbordercolor },
+		{ "normfgcolor"     , STRING  , &normfgcolor     },
+		{ "selbgcolor"      , STRING  , &selbgcolor      },
+		{ "selbordercolor"  , STRING  , &selbordercolor  },
+		{ "selfgcolor"      , STRING  , &selfgcolor      },
+		{ "borderpx"        , INTEGER , &borderpx        },
+		{ "snap"            , INTEGER , &snap            },
+		{ "showbar"         , INTEGER , &showbar         },
+		{ "topbar"          , INTEGER , &topbar          },
+		{ "nmaster"         , INTEGER , &nmaster         },
+		{ "resizehints"     , INTEGER , &resizehints     },
+		{ "mfact"           , FLOAT   , &mfact           },
 };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // tile
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} }, // floating
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // monocle
-	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} }, // deck
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, // centeredmaster
-	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[5]} }, // centeredfloatingmaster
-	{ MODKEY,                       XK_p,      setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_p,      togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_n,      togglealttag,   {0} },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	/* modifier          key        function          argument */
+	{ MODKEY           , XK_space  , spawn          , {.v = dmenucmd   }  },
+	{ MODKEY           , XK_Return , spawn          , {.v = termcmd    }  },
+	{ MODKEY           , XK_b      , togglebar      , {0               }  },
+	{ MODKEY           , XK_j      , focusstack     , {.i = +1         }  },
+	{ MODKEY           , XK_k      , focusstack     , {.i = -1         }  },
+	{ MODKEY           , XK_i      , incnmaster     , {.i = +1         }  },
+	{ MODKEY           , XK_d      , incnmaster     , {.i = -1         }  },
+	{ MODKEY           , XK_h      , setmfact       , {.f = -0.05      }  },
+	{ MODKEY           , XK_l      , setmfact       , {.f = +0.05      }  },
+	{ MODKEY           , XK_Return , zoom           , {0               }  },
+	{ MODKEY           , XK_Tab    , view           , {0               }  },
+	{ MODKEY           , XK_q      , killclient     , {0               }  },
+	{ MODKEY           , XK_t      , setlayout      , {.v = &layouts[0]}  }, // tile
+	{ MODKEY|ShiftMask , XK_f      , setlayout      , {.v = &layouts[1]}  }, // floating
+	{ MODKEY           , XK_m      , setlayout      , {.v = &layouts[2]}  }, // monocle
+	{ MODKEY           , XK_c      , setlayout      , {.v = &layouts[3]}  }, // deck
+	{ MODKEY           , XK_u      , setlayout      , {.v = &layouts[4]}  }, // centeredmaster
+	{ MODKEY|ShiftMask , XK_u      , setlayout      , {.v = &layouts[5]}  }, // centeredfloatingmaster
+	{ MODKEY           , XK_p      , setlayout      , {0               }  },
+	{ MODKEY|ShiftMask , XK_p      , togglefloating , {0               }  },
+	{ MODKEY           , XK_0      , view           , {.ui = ~0        }  },
+	{ MODKEY|ShiftMask , XK_0      , tag            , {.ui = ~0        }  },
+	{ MODKEY           , XK_comma  , focusmon       , {.i = -1         }  },
+	{ MODKEY           , XK_period , focusmon       , {.i = +1         }  },
+	{ MODKEY|ShiftMask , XK_comma  , tagmon         , {.i = -1         }  },
+	{ MODKEY|ShiftMask , XK_period , tagmon         , {.i = +1         }  },
+	{ MODKEY           , XK_n      , togglealttag   , {0               }  },
+	{ MODKEY           , XK_minus  , setgaps        , {.i = -5         }  },
+	{ MODKEY           , XK_equal  , setgaps        , {.i = +5         }  },
+	{ MODKEY|ShiftMask , XK_equal  , setgaps        , {.i = 0          }  },
+	{ MODKEY|ShiftMask , XK_q      , quit           , {0               }  },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -128,23 +129,22 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click      event mask   button    function          argument */
+	{ ClkLtSymbol   , 0      , Button1 , setlayout      , {0                }  },
+	{ ClkLtSymbol   , 0      , Button3 , setlayout      , {.v = &layouts[2] }  },
+	{ ClkWinTitle   , 0      , Button2 , zoom           , {0                }  },
+	{ ClkStatusText , 0      , Button2 , spawn          , {.v = termcmd     }  },
+	{ ClkClientWin  , MODKEY , Button1 , movemouse      , {0                }  },
+	{ ClkClientWin  , MODKEY , Button2 , togglefloating , {0                }  },
+	{ ClkClientWin  , MODKEY , Button3 , resizemouse    , {0                }  },
+	{ ClkTagBar     , 0      , Button1 , view           , {0                }  },
+	{ ClkTagBar     , 0      , Button3 , toggleview     , {0                }  },
+	{ ClkTagBar     , MODKEY , Button1 , tag            , {0                }  },
+	{ ClkTagBar     , MODKEY , Button3 , toggletag      , {0                }  },
 };
 
