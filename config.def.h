fb67be227af1431c5650efde21bdad2d222998e3
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-10-01 15:42:33 -0600

    Fix: removed trayer from autostart

--- config.def.h
+++ config.def.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "trayer",  NULL,     NULL,          ~0,         True,       1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
